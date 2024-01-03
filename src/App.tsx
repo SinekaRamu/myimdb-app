74e21524357f2499dbc9c7254ffd5c6f44220ba1
SinekaRamu <sineka2001cud@gmail.com>
2024-01-03 10:19:14 +0530

    private routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotfoundPage from "./pages/NotfoundPage";
 import Home from "./pages/HomePage";
+import PrivateRoutes from "./services/PrivateRoutes";
 const Signup = lazy(() => import("./pages/SignupPage"));
 const Login = lazy(() => import("./pages/LoginPage"));
 const Account = lazy(() => import("./pages/ViewAccountPage"));
@@ -18,10 +19,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/u/account" element={<Account />} />
-          <Route path="/addMovie" element={<AddMovie />} />
-          <Route path="/movies/:id" element={<Movie />} />
-          <Route path="/movies/:id/rating" element={<AddRating />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/u/account" element={<Account />} />
+            <Route path="/addMovie" element={<AddMovie />} />
+            <Route path="/movies/:id" element={<Movie />} />
+            <Route path="/movies/:id/rating" element={<AddRating />} />
+          </Route>
           <Route path="*" element={<NotfoundPage />} />
         </Routes>
       </BrowserRouter>
